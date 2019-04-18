1cce06f843c6ed827c0a7070bffccbb956aee6ed
David Arnold <dar@xoe.solutions>
2019-04-18 18:29:49 -0500

    [IMP] allow empty src

--- v-10.0/out/entrypoint.d/10-set-appenv.sh
+++ v-10.0/out/entrypoint.d/10-set-appenv.sh
@@ -8,14 +8,27 @@ set -Eeuo pipefail
 
 export ODOO_ADMINPASSWORD_FILE="${ODOO_ADMINPASSWORD_FILE:=/run/secrets/adminpwd}"  # Odoo Passfile (Patch tools/0002)
 
-addonspath="${ODOO_SRC}"
+addonspath=""
 # Sort reverse alfanumerically first, then do realpath
 # so we can freely reorder loading by symlinking for
 # exemple in a CI environment directly from a git clone.
 
+for dir in $(find "${ODOO_SRC}" -maxdepth 3 -exec test -e {}/__manifest__.py -o -e {}/__openerp__.py \; -exec dirname {} \; | uniq | sort | xargs realpath --no-symlinks); do
+    echo "==>  Adding $dir to addons path"
+    if [ -z "$addonspath" ]; then
+        addonspath=$dir
+    else
+        addonspath=$addonspath,$dir
+    fi;
+done;
+
 for dir in $(find "${ODOO_VENDOR}" -maxdepth 5 -exec test -e {}/__manifest__.py -o -e {}/__openerp__.py \; -exec dirname {} \; | uniq | sort | xargs realpath --no-symlinks); do
 
     echo "==>  Adding $dir to addons path"
-    addonspath=$addonspath,$dir
+    if [ -z "$addonspath" ]; then
+        addonspath=$dir
+    else
+        addonspath=$addonspath,$dir
+    fi;
 done;
 export ODOO_ADDONS_PATH=$addonspath
